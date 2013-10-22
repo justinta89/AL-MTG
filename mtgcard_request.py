6c1c097c5285a4244b3f0f881056f25cbaa71060
Justin Anderson <justin.ta@outlook.com>
2013-10-22 16:00:29 -0600

    Update mtgcard_request.py

--- mtgcard_request.py
+++ mtgcard_request.py
@@ -3,12 +3,16 @@ def mtgcard_request(cardname, cardset):
    import requests
    import json
 
+   # The cardset is optional, but if included it will give the most accurate results
    r = requests.get('http://magictcgprices.appspot.com/api/cfb/price.json?cardname=%s&cardset=%s' % (cardname, cardset))
    data = json.loads(r.text)
-   # ebay:  http://magictcgprices.appspot.com/api/ebay/price.json?cardname=%s&cardset=%s
-   # tcgplayer:  http://magictcgprices.appspot.com/api/tcgplayer/price.json?cardname=%s&cardset=%s
-   # channel fireball:  http://magictcgprices.appspot.com/api/cfb/price.json?cardname=%s&setname=%s
-
+   """
+   Different places to check card prices, all using magictcgprices api:
+   	ebay:  http://magictcgprices.appspot.com/api/ebay/price.json?cardname=%s&cardset=%s
+   	tcgplayer:  http://magictcgprices.appspot.com/api/tcgplayer/price.json?cardname=%s&cardset=%s
+   	channel fireball:  http://magictcgprices.appspot.com/api/cfb/price.json?cardname=%s&setname=%s
+   """
+   
    price = data[0]
 
    return "Price of Card: %s" % price
