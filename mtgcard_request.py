fae54268f67b9cccc18136ed59a542732bb47081
Justin Anderson <justin.ta@outlook.com>
2013-10-24 10:40:30 -0600

    Alpha code?

--- mtgcard_request.py
+++ mtgcard_request.py
@@ -3,22 +3,34 @@ def mtgcard_request(cardname, cardset):
    import requests
    import json
 
-   # The cardset is optional, but if included it will give the most accurate results
-   response = requests.get('http://magictcgprices.appspot.com/api/tcgplayer/price.json?cardname=%s&cardset=%s' % (cardname, cardset)) # Card Image
-   data = json.loads(response.text)
+   # Card Image
+   ci_response = requests.get('http://magictcgprices.appspot.com/api/tcgplayer/price.json?cardname=%s&cardset=%s' % (cardname, cardset))
+   ci_data = json.loads(ci_response.text)
 
-   price = "Low: %s \nMedium: %s \nHigh: %s" % (data[0], data[1], data[2])
+   # Ebay (lowest price)
+   ebay_response = requests.get('http://magictcgprices.appspot.com/api/ebay/price.json?cardname=%s&cardset=%s' % (cardname, cardset))
+   ebay_data = json.loads(ebay_response.text)
 
-   """
-   Different places to check card prices, all using magictcgprices api:
-           card image: http://magictcgprices.appspot.com/api/cfb/price.json?cardname=%s&cardset=%s  # mainly using this one. Outputs med price
-           ebay:  http://magictcgprices.appspot.com/api/ebay/price.json?cardname=%s&cardset=%s
-           tcgplayer:  http://magictcgprices.appspot.com/api/tcgplayer/price.json?cardname=%s&cardset=%s
-                This one outputs low, medium, and high prices. 
-           channel fireball:  http://magictcgprices.appspot.com/api/cfb/price.json?cardname=%s&setname=%s
-   """
+   # Channel Fireball
+   cfb_response = requests.get('http://magictcgprices.appspot.com/api/cfb/price.json?cardname=%s&setname=%s' % (cardname, cardset))
+   cfb_data = json.loads(cfb_response.text)
 
-   return price
+   # TCGPlayer, this gives 3 outputs. low, med, high
+   tcgplayer_response = requests.get('http://magictcgprices.appspot.com/api/tcgplayer/price.json?cardname=%s&cardset=%s' % (cardname, cardset))
+   tcg_data = json.loads(tcgplayer_response.text)
+
+
+   # Set variable for each different api location
+   ci_price = "\nCard Image: %s" % (ci_data[0])
+   ebay_price = "\nEbay (Lowest Price): %s" % (ebay_data[0])
+   cfb_price = "\nChannel Fireball: %s" % (cfb_data[0])
+   tcg_price = "\nTCGPlayer: \nLow: %s \nMedium: %s \nHigh: %s" % (tcg_data[0], tcg_data[1], tcg_data[2])
+   
+   all_prices = [ci_price, ebay_price, cfb_price, tcg_price]
+   
+   for ap in all_prices:
+   	return ap
+   
 
 if __name__ == "__main__":
 	import sys
