bd43f36806fd75cfb4861f89815a1c15375f3712
Justin Anderson <justin.ta@outlook.com>
2013-10-23 21:04:37 -0600

    Update mtgcard_request.py

--- mtgcard_request.py
+++ mtgcard_request.py
@@ -4,18 +4,21 @@ def mtgcard_request(cardname, cardset):
    import json
 
    # The cardset is optional, but if included it will give the most accurate results
-   r = requests.get('http://magictcgprices.appspot.com/api/cfb/price.json?cardname=%s&cardset=%s' % (cardname, cardset))
-   data = json.loads(r.text)
+   response = requests.get('http://magictcgprices.appspot.com/api/tcgplayer/price.json?cardname=%s&cardset=%s' % (cardname, cardset)) # Card Image
+   data = json.loads(response.text)
+
+   price = "Low: %s \nMedium: %s \nHigh: %s" % data[0], data[1], data[2]
+
    """
    Different places to check card prices, all using magictcgprices api:
-   	ebay:  http://magictcgprices.appspot.com/api/ebay/price.json?cardname=%s&cardset=%s
-   	tcgplayer:  http://magictcgprices.appspot.com/api/tcgplayer/price.json?cardname=%s&cardset=%s
-   	channel fireball:  http://magictcgprices.appspot.com/api/cfb/price.json?cardname=%s&setname=%s
+           card image: http://magictcgprices.appspot.com/api/cfb/price.json?cardname=%s&cardset=%s  # mainly using this one. Outputs med price
+           ebay:  http://magictcgprices.appspot.com/api/ebay/price.json?cardname=%s&cardset=%s
+           tcgplayer:  http://magictcgprices.appspot.com/api/tcgplayer/price.json?cardname=%s&cardset=%s
+                This one outputs low, medium, and high prices. 
+           channel fireball:  http://magictcgprices.appspot.com/api/cfb/price.json?cardname=%s&setname=%s
    """
-   
-   price = data[0]
 
-   return "Price of Card: %s" % price
+   return price
 
 if __name__ == "__main__":
 	import sys
