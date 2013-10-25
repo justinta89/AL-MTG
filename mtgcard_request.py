ed6bf324ef664958a308a77a7168b5e8ed76bc22
Justin Anderson <justin.ta@outlook.com>
2013-10-25 13:57:23 -0600

    Update mtgcard_request.py

--- mtgcard_request.py
+++ mtgcard_request.py
@@ -1,19 +1,11 @@
-def mtgcard_request(cardname, cardset):
+def mtgCardRequest(cardname, cardset):
   
    import requests
    import json
 
-   # Card Image
-   ci_response = requests.get('http://magictcgprices.appspot.com/api/tcgplayer/price.json?cardname=%s&cardset=%s' % (cardname, cardset))
-   ci_data = json.loads(ci_response.text)
-
    # Ebay (lowest price)
    ebay_response = requests.get('http://magictcgprices.appspot.com/api/ebay/price.json?cardname=%s&cardset=%s' % (cardname, cardset))
    ebay_data = json.loads(ebay_response.text)
-
-   # Channel Fireball
-   cfb_response = requests.get('http://magictcgprices.appspot.com/api/cfb/price.json?cardname=%s&setname=%s' % (cardname, cardset))
-   cfb_data = json.loads(cfb_response.text)
 
    # TCGPlayer, this gives 3 outputs. low, med, high
    tcgplayer_response = requests.get('http://magictcgprices.appspot.com/api/tcgplayer/price.json?cardname=%s&cardset=%s' % (cardname, cardset))
@@ -21,15 +13,20 @@ def mtgcard_request(cardname, cardset):
 
 
    # Set variable for each different api location
-   ci_price = "\nCard Image: %s" % (ci_data[0])
-   ebay_price = "\nEbay (Lowest Price): %s" % (ebay_data[0])
-   cfb_price = "\nChannel Fireball: %s" % (cfb_data[0])
-   tcg_price = "\nTCGPlayer: \nLow: %s \nMedium: %s \nHigh: %s" % (tcg_data[0], tcg_data[1], tcg_data[2])
-   
-   all_prices = [ci_price, ebay_price, cfb_price, tcg_price]
-   
-   for ap in all_prices:
-   	return ap
+   ebay_price = ebay_data[0]
+   tcg_low = tcg_data[0]
+   tcg_med = tcg_data[1]
+   tcg_high = tcg_data[2]
+
+   # Price dictionary
+   all_prices = {
+         'Ebay': ebay_price,
+         'TCGPlayer Low': tcg_low,
+         'TCGPlayer Median': tcg_med,
+         'TCGPlayer High': tcg_high
+   }
+
+   return all_prices
    
 
 if __name__ == "__main__":
@@ -38,4 +35,4 @@ if __name__ == "__main__":
 	# cardset = sys.argv[2]
 	cardname = raw_input("What card do you want? ")
 	cardset = raw_input("What set is it in? ")
-	print mtgcard_request(cardname, cardset)
+	print mtgCardRequest(cardname, cardset)
