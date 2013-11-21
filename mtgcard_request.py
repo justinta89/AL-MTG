157ba4dd033f7718ad55fdd5bc8861569df21f3d
Justin Anderson <justin.ta@outlook.com>
2013-11-21 07:09:16 -0700

    Update mtgcard_request.py

--- mtgcard_request.py
+++ mtgcard_request.py
@@ -1,39 +1,21 @@
-def mtgCardRequest(cardname, cardset):
+def mtgCardRequest(cardname):
   
    import requests
    import json
 
-   # Ebay (lowest price)
-   ebay_response = requests.get('http://magictcgprices.appspot.com/api/ebay/price.json?cardname=%s&cardset=%s' % (cardname, cardset))
-   ebay_data = json.loads(ebay_response.text)
-
    # TCGPlayer, this gives 3 outputs. low, med, high
-   tcgplayer_response = requests.get('http://magictcgprices.appspot.com/api/tcgplayer/price.json?cardname=%s&cardset=%s' % (cardname, cardset))
+   tcgplayer_response = requests.get('http://magictcgprices.appspot.com/api/tcgplayer/price.json?cardname=%s' % cardname)
    tcg_data = json.loads(tcgplayer_response.text)
 
-   # Set variable for each different api location
-   # ebay_price = ebay_data[0]
    tcg_low = tcg_data[0]
    tcg_med = tcg_data[1]
    tcg_high = tcg_data[2]
 
    # Price dictionary
-   if tcg_data['magic']:
-      response = {
-         # 'Ebay': ebay_price,
-         'tcgplayer_low': tcg_low,
-         'tcplayer_medium': tcg_med,
-         'tcgplayer_high': tcg_high
-      }
-   else:
-      response = None
-
-   # Price dictionary, originally how I had it. Works, I don't thnk the above does though...
    all_prices = {
-         'Ebay': ebay_price,
-         'TCGPlayer Low': tcg_low,
-         'TCGPlayer Median': tcg_med,
-         'TCGPlayer High': tcg_high
+         'Low': tcg_low,
+         'Median': tcg_med,
+         'High': tcg_high
    }
 
    return all_prices
@@ -43,5 +25,4 @@ if __name__ == "__main__":
 	# cardname = sys.argv[1]
 	# cardset = sys.argv[2]
 	cardname = raw_input("What card do you want? ")
-	cardset = raw_input("What set is it in? ")
-	print mtgCardRequest(cardname, cardset)
+	print mtgCardRequest(cardname)
